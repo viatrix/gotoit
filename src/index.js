fbcf76db9e888abdab3c36e504a94a07746e5e90
gapak <gapak@adswings.com>
2016-12-16 10:42:33 +0200

    bootstrap loader dont help)

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+//import 'bootstrap-loader';
+//import 'bootstrap';
+//import 'bootstrap-loader';
+//import 'bootstrap/dist/css/bootstrap.css';
 
-import 'bootstrap/dist/css/bootstrap.css';
-
-//import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
 //import '../node_modules/bootstrap/dist/js/bootstrap.js';
 
 //import react-bootstrap from 'react-bootstrap';// 'bootstrap/dist/css/bootstrap.css';
