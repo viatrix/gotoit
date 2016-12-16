4f2fd33aaed2c183e67b990bba4066f888ced5bd
gapak <gapak@adswings.com>
2016-12-16 10:14:23 +0200

    bootstrap glitch

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-//import $ from "jquery";
-
 import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/css/bootstrap-theme.css';
+
+//import '../node_modules/bootstrap/dist/css/bootstrap.css';
+//import '../node_modules/bootstrap/dist/js/bootstrap.js';
+
+//import react-bootstrap from 'react-bootstrap';// 'bootstrap/dist/css/bootstrap.css';
+
+
+//import 'bootstrap/dist/css/bootstrap-theme.css';
 //import '../node_modules/bootstrap/dist/js/bootstrap.js';
 
 //import 'react-bootstrap';
 
+//import $ from "jquery";
 //window.jQuery = jQuery = window.$ = $;
 //console.log($);
 
