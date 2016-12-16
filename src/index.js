628217adc9784ecbd49d89f0c2675bfd9c6a235a
gapak <gapak@adswings.com>
2016-12-16 10:48:52 +0200

    bootstrap from cdn

--- src/index.js
+++ src/index.js
@@ -1,22 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-//import 'bootstrap-loader';
-//import 'bootstrap';
-//import 'bootstrap-loader';
-//import 'bootstrap/dist/css/bootstrap.css';
-
-import '../node_modules/bootstrap/dist/css/bootstrap.css';
-//import '../node_modules/bootstrap/dist/js/bootstrap.js';
-
-//import react-bootstrap from 'react-bootstrap';// 'bootstrap/dist/css/bootstrap.css';
-
-
-//import 'bootstrap/dist/css/bootstrap-theme.css';
-//import '../node_modules/bootstrap/dist/js/bootstrap.js';
-
-//import 'react-bootstrap';
-
 //import $ from "jquery";
 //window.jQuery = jQuery = window.$ = $;
 //console.log($);
