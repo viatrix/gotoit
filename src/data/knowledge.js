c830d03daaca78ed1373bf411fd0234248d6a8a0
Alex Gapak <iamgapak@gmail.com>
2016-12-21 07:01:32 +0200

    Start Money little boost

--- src/data/knowledge.js
+++ src/data/knowledge.js
@@ -32,15 +32,15 @@ export const education = {
 
 
 export const player_backgrounds = {
-    specialist: {name: 'Specialist', money: 2000, text: 'Honed skills in the profession to heights. Besides richer.'},
-    comprehensive: {name: 'Comprehensive', money: 0, text: 'Has no weaknesses. But is not the special.'},
-    coworker: {name: 'Coworker', money: 1000, text: 'Works with the partner, covering each other.'}
+    specialist: {name: 'Specialist', money: 4000, text: 'Honed skills in the profession to heights. Besides richer.'},
+    comprehensive: {name: 'Comprehensive', money: 1000, text: 'Has no weaknesses. But is not the special.'},
+    coworker: {name: 'Coworker', money: 3000, text: 'Works with the partner, covering each other.'}
 };
 
 export const player_education = {
-    autodidact: {name: 'Autodidact', money: 0, text: 'Inspired researcher, looking own way. Eclectic stats.'},
-    university: {name: 'University', money: 1000, text: 'Fundamental education according to verified program. Flat stats.'},
-    businessman: {name: 'Businessman', money: 3000, text: 'Made a fortune doing business. More money.'}
+    autodidact: {name: 'Autodidact', money: 1000, text: 'Inspired researcher, looking own way. Eclectic stats.'},
+    university: {name: 'University', money: 3000, text: 'Fundamental education according to verified program. Flat stats.'},
+    businessman: {name: 'Businessman', money: 6000, text: 'Made a fortune doing business. More money.'}
 };
 
 export default {};
